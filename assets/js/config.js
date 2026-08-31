a704666608ec747e5c55dc54c1cd704e136b509d
slfubu <thapanutputtathakorn@gmail.com>
2026-09-01 03:12:49 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,7 +6,7 @@
  * 3) Keep the URL ending in /exec (not /dev).
  */
 window.APP_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbxu4r1cOKJR9Sf8PCh-6EaFOV6KNiotNTUXat3momVVUgiSwDzDwc9kmBOL8tLJIE3PdA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzzYuTZlMioe52HPBtlGa-YeA8QUtnZhDu_xda05pav5YilnE2X7hUEYRQWR6tlKZdEwQ/exec',
   REQUEST_TIMEOUT_MS: 120000,
   DEBUG_API: false
 });
